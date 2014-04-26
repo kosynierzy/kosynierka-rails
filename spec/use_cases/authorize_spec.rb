2544188a18e11016ab8d33e043035cd44b68c9cf
Jan Dudulski <jan@dudulski.pl>
2014-04-26 23:03:32 +0200

    [test] Spec Authorize use case

---
+++ spec/use_cases/authorize_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe Authorize do
+  subject { described_class.new(id, info, options) }
+
+  let(:info) do
+    {
+      email: email,
+      username: username
+    }
+  end
+  let(:options) { {user_repo: user_repo} }
+  let(:email) { 'test@example.com' }
+  let(:username) { 'test' }
+  let(:user_repo) { UserRepoMock }
+
+  describe '#call' do
+    let(:result) { subject.call }
+
+    context 'when user is known' do
+      let(:id) { 'user-1' }
+
+      it 'returns updated user' do
+        expect(result.id).to eq(id)
+        expect(result.username).to eq(username)
+        expect(result.email).to eq(email)
+      end
+    end
+
+    context 'when user is not known' do
+      let(:id) { 'user-0' }
+
+      it 'returns new user' do
+        expect(result.id).to eq(id)
+        expect(result.username).to eq(username)
+        expect(result.email).to eq(email)
+      end
+    end
+  end
+
+  class UserRepoMock
+    def self.find_by(attrs)
+      if attrs.fetch(:id) == 'user-1'
+        UserMock.new(id: 'user-1', username: 'double', email: 'double@example.com')
+      else
+        nil
+      end
+    end
+
+    def self.new(attrs)
+      user = UserMock.new(attrs)
+      yield user if block_given?
+      user
+    end
+  end
+
+  class UserMock
+    attr_accessor :id, :email, :username
+
+    def initialize(attrs)
+      update_attributes!(attrs)
+    end
+
+    def update_attributes!(attrs)
+      attrs.each do |k, v|
+        send("#{k}=", v)
+      end
+    end
+
+    def save!
+    end
+  end
+end
